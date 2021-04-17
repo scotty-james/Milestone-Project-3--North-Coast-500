de7dbb3b8a6c6e56ae804a22a0f69e905bea6673
scotty-james <sfrew26@gmail.com>
2021-04-17 13:36:50 +0000

    Create Sign Up Form

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
+from forms import SignupForm
 if os.path.exists("env.py"):
     import env
 
@@ -27,6 +29,35 @@ def home():
 def get_posts():
     posts = mongo.db.posts.find()
     return render_template("posts.html", posts=posts)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    """Handles registration functionality"""
+    form = SignupForm(request.form)
+    if form.validate_on_submit():
+        # get all the users
+        existing_users = mongo.db.users
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register", form=form))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
+
+    return render_template("register.html", form=form)
 
 
 if __name__ == "__main__":
---
+++ forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, SelectField
+from wtforms.validators import DataRequired, Length, EqualTo
+
+
+class SignupForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign Up')
