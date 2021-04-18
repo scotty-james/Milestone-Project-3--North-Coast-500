1d74fef912e166162cb3417a4b8f5ca6943bf4f3
scotty-james <sfrew26@gmail.com>
2021-04-18 15:22:59 +0000

    Add footer to base template

--- forms.py
+++ forms.py
@@ -6,14 +6,12 @@ from wtforms.validators import DataRequired, Length, EqualTo
 class SignupForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign Up')
 
 
 class SigninForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
 
